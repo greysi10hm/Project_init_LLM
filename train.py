44c0038205388900183eace9640731b5dc9c44bb
brenda <greysi10hm@gmail.com>
2024-03-04 23:49:34 +0100

    ho salvato le ultime modifiche

--- train.py
+++ train.py
@@ -6,4 +6,7 @@ import tiktoken
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
+#Creazione e addestramento
 openai.api_key  = os.environ['OPENAI_API_KEY']
+
+#test del modello addestrato
